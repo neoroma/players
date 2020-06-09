81e9d40574e9f83b1918bb084168aec32b56f80f
Roman Shestakov <neoroma@gmail.com>
2020-06-09 11:34:27 +0300

    Add some inital changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './ao_logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const players = [
+  { name: 'Rafael Nadal', id: 1 },
+  { name: 'Roger Federer', id: 2 },
+  { name: 'Novak Djokovic', id: 3 },
+]
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      players,
+    }
+
+    this.fixText = this.fixText.bind(this)
+    this.renderPlayers = this.renderPlayers.bind(this)
+  }
+
+  fixText() {
+    this.setState({ text: `lololo ${Date.now()}` })
+  }
+
+  renderPlayers() {
+    return (
+      <ul>
+        {this.state.players.map(({ name, id }) => (
+          <li key={id}>{name}</li>
+        ))}
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {this.renderPlayers()}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
