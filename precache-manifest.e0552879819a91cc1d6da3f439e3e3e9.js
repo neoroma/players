0eadc58e0c5245e3b27b3881f751d09fc6f15e9e
Roman Shestakov <neoroma@gmail.com>
2020-06-11 16:55:21 +0300

    Updates

---
+++ precache-manifest.e0552879819a91cc1d6da3f439e3e3e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1064f1378d6e164706169a365b4a74",
+    "url": "/players/index.html"
+  },
+  {
+    "revision": "e7f84a0c71a96852d5da",
+    "url": "/players/static/css/main.2a38be14.chunk.css"
+  },
+  {
+    "revision": "31c6df3c89b4ae0dac67",
+    "url": "/players/static/js/2.5c0c4fda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/players/static/js/2.5c0c4fda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f84a0c71a96852d5da",
+    "url": "/players/static/js/main.d0ee3038.chunk.js"
+  },
+  {
+    "revision": "1b78a40f0dcb32f114c2",
+    "url": "/players/static/js/runtime-main.dbde3b56.js"
+  },
+  {
+    "revision": "9405910bf008203c37c83559bb0781ab",
+    "url": "/players/static/media/ao_logo.9405910b.svg"
+  }
+]);
