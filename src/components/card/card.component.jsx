8b9e5033e3bdb151100f0729e1f6c8af374d8d8d
Roman Shestakov <neoroma@gmail.com>
2020-06-11 17:12:08 +0300

    Move back the image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './card.style.css'
-import image from '../../fed.jpg'
 
 export const Card = ({ details }) => {
   const { name, gender } = details
   return (
     <div className="card-item">
       <div className="image">
-        <img src={image} alt={name} />
+        <img src="/fed.jpg'" alt={name} />
       </div>
       <div className="name">{name}</div>
       <div className="gender">{gender}</div>
