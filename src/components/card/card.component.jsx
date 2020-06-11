db4dffa68fe32cebc86a242381cca867075398c9
Roman Shestakov <neoroma@gmail.com>
2020-06-11 17:06:16 +0300

    Move the fed image

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './card.style.css'
+import image from '../../fed.jpg'
 
 export const Card = ({ details }) => {
   const { name, gender } = details
   return (
     <div className="card-item">
       <div className="image">
-        <img src="/fed.jpg" alt={name} />
+        <img src={image} alt={name} />
       </div>
       <div className="name">{name}</div>
       <div className="gender">{gender}</div>
